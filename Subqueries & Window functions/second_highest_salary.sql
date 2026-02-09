f5c549f46e083a9ce8550d92045a64e294e51e2f
Gayathri Ramesh <gayathriramesh2324@gmail.com>
2026-02-09 19:05:21 +0530

    Create second_highest_salary.sql
    This is to find the second highest salary among employees,
    I wrote using both subqueries and window functions

---
+++ Subqueries & Window functions/second_highest_salary.sql
@@ -0,0 +1,21 @@
+# To find the second highest salary among employees
+
+--Using Subquery:
+Select MAX(salary) as SecondHighestSalary
+from Employee
+where salary < 
+    (
+        Select MAX(salary) 
+        from Employee
+    )
+
+--Using Window function:
+With cte_rank as (
+Select id,salary,
+DENSE_RANK() OVER(ORDER BY salary DESC) rnk
+from Employee
+)
+
+Select MAX(salary) as SecondHighestSalary
+from cte_rank
+where rnk = 2
