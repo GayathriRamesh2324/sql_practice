37224f7543285f1ae8b5a2a78ba095285196aeda
Gayathri Ramesh <gayathriramesh2324@gmail.com>
2026-02-18 12:51:16 +0530

    Update Replace_unique_identifier.sql

--- Joins/Replace_unique_identifier.sql
+++ Joins/Replace_unique_identifier.sql
@@ -1,1 +1,39 @@
+# to show the unique ID of each user, If a user does not have a unique ID replace just show null.
+Input: 
+Employees table:
++----+----------+
+| id | name     |
++----+----------+
+| 1  | Alice    |
+| 7  | Bob      |
+| 11 | Meir     |
+| 90 | Winston  |
+| 3  | Jonathan |
++----+----------+
+EmployeeUNI table:
++----+-----------+
+| id | unique_id |
++----+-----------+
+| 3  | 1         |
+| 11 | 2         |
+| 90 | 3         |
++----+-----------+
 
+
+Select u.unique_id,e.name
+from Employees e
+LEFT JOIN EmployeeUNI u
+ON e.id = u.id
+
+Output: 
++-----------+----------+
+| unique_id | name     |
++-----------+----------+
+| null      | Alice    |
+| null      | Bob      |
+| 2         | Meir     |
+| 3         | Winston  |
+| 1         | Jonathan |
++-----------+----------+
+
+  
